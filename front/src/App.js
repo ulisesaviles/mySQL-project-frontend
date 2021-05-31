31b774710d0b4a6f569d773012a780b91212439d
Ulises Aviles <ulises.aviles@cetys.edu.mx>
2021-05-30 18:50:35 -0700

    Setup and headers are done

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// React imports
+import { useState } from "react";
 
-function App() {
+// Local imports
+import logo from "./img/logo.png";
+import queries from "./config/queries";
+
+// Styles
+import "./App.css";
+
+const App = () => {
+  const [selectedQueryIndex, setSelectedQueryIndex] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <img src={logo} className="logo" alt="logo" />
       </header>
+      <nav>
+        {queries.map((query) => (
+          <div
+            onClick={() => {
+              setSelectedQueryIndex(queries.indexOf(query));
+            }}
+            className={
+              queries.indexOf(query) === selectedQueryIndex
+                ? "navBtn navBtnSelected"
+                : "navBtn"
+            }
+          >
+            Consulta {queries.indexOf(query) + 1}
+          </div>
+        ))}
+      </nav>
+      <div className="content">
+        <div className="contentContainer">
+          <div className="contentHeader">
+            <h1 className="contentTitle">Consulta {selectedQueryIndex + 1}</h1>
+            <h2 className="descriptionTitle">Descripción de la consulta:</h2>
+            <p className="description">
+              {queries[selectedQueryIndex].description}
+            </p>
+          </div>
+          <div className="queriesContainer">
+            <div className="contentSubsection contentSubsection1"></div>
+            <div className="contentSubsection contentSubsection2"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ front/src/config/queries.js
@@ -0,0 +1,182 @@
+export default [
+  {
+    //1
+    type: "dates",
+    description:
+      "Mostrar la informacion del cliente con mayor frecuencia en reparaciones que se le hayan atendido en un periodo de tiempo.",
+  },
+  {
+    //2
+    type: "dates",
+    description:
+      "Mostrar la informacion de las promociones aplicadas en el departamento mecanico en un periodo de fechas especifico.",
+  },
+  {
+    //3
+    type: "departments",
+    description:
+      "Mostrar la informacion del inventario de refacciones que tiene cada producto registrado.",
+  },
+  {
+    //4
+    type: null,
+    description:
+      "Mostrar las ventas obtenidas con respecto al serivicio de solo diagnosticos.",
+  },
+  {
+    //5
+    type: "dates",
+    description:
+      "Mostrar entre  el periodo de rango de fechas el numero de atenciones para cada departamento del taller.",
+  },
+  {
+    //6
+    type: "departments",
+    description:
+      "Mostrar las ganacias obtenidas en un rango de fechas especifico en base a un despartamento en particular.",
+  },
+  {
+    //7
+    type: "dates",
+    description:
+      "Mostrar el empleado que mayor ganancia obtuvo en un rango de fechas especifico.",
+  },
+  {
+    //8
+    type: "departments",
+    description:
+      "Mostrar e listado de supervisores y sus supervisados en base a un departamento especifico.",
+  },
+  {
+    //9
+    type: null,
+    description:
+      "Mostrar el listado de clientes registrados junto con los automoviles que tiene registrados en sistema.",
+  },
+  {
+    //10
+    type: "departments",
+    description:
+      "Mostrar las reparaciones mas concurrentes en base a un departamento especifico.",
+  },
+  {
+    //11
+    type: "dates",
+    description:
+      "Mostrar el departamento que presenta el mayor consumo de refacciones en un periodo de fecha determinado y muestr el numero de refacciones utilizadas.",
+  },
+  {
+    //12
+    type: "dates",
+    description:
+      "Mostrar las promociones vigentes por un rango de fechas determinado.",
+  },
+  {
+    //13
+    type: "dates",
+    description:
+      "Mostrar las garantias que se han presentado en los servicios y que costo ha generado por un rango de fechas especifico.",
+  },
+  {
+    //14
+    type: "dates",
+    description:
+      "Mostrar el departameto con mayor afluencia de reparaciones en un rango de fechas determinado.",
+  },
+  {
+    //15
+    type: "dates",
+    description:
+      "Mostrar el departameto con menor afluencia de reparaciones por un rango de fecha determinado.",
+  },
+  {
+    //16
+    type: null,
+    description:
+      "Mostrar el consumo de refacciones clasificado por departamento.",
+  },
+  {
+    //17
+    type: "dates",
+    description:
+      "Mostrar las ganacias obtenidas del taller por un rango de fechas determinado.",
+  },
+  {
+    //18
+    type: null,
+    description:
+      "Mostrar el listado de empleados clasificado por departamentos.",
+  },
+  {
+    //19
+    type: "dates",
+    description:
+      "Mostrar el las caracteristicas de los automoviles registrado en un periodo de tiempo determinado.",
+  },
+  {
+    //20
+    type: "dates",
+    description:
+      "Mostrar los clientes que solo realizaron servicios de diagnostico en un periodo de fecha determinado.",
+  },
+  {
+    //21
+    type: null,
+    description:
+      "Mostrar el cliente con el mayor gasto en reparaciones realizado.",
+  },
+  {
+    //22
+    type: "dates",
+    description:
+      "Mostrar el automovil con el mayor gasto realizado en un periodo de tiempo determinado.",
+  },
+  {
+    //23
+    type: null,
+    description:
+      "Mostrar cual es la refaccion mas vendida del departamento de refacciones y cuanto genera de ganancia.",
+  },
+  {
+    //24
+    type: null,
+    description:
+      "Mostrar el departamento con mayor numero de garantias aplicadas y cuanto suma de gasto debido a dichas garantias.",
+  },
+  {
+    //25
+    type: "dates",
+    description:
+      "Mostrar el cliente con mayor tiempo de no visitar el taller en base a un rango de fechas determiando.",
+  },
+  {
+    //26
+    type: "dates",
+    description:
+      "Mostrar la promocion mas aplicada en un rango de fechas determinado.",
+  },
+  {
+    //27
+    type: "dates",
+    description:
+      "Mostrar el numero de promociones vencidas en un rango de fechas determinado.",
+  },
+  {
+    //28
+    type: null,
+    description:
+      "Mostrar los datos del trabajador con mayor garantias aplicadas de los servicio que realizo.",
+  },
+  {
+    //29
+    type: "dates",
+    description:
+      "Mostrar el nombre de cliente y el nombre del automovil que haya presentado el mayor numero de servicios aplicados en un periodo de fechas determinado.",
+  },
+  {
+    //30
+    type: "dates",
+    description:
+      "Mostrar los servicios con menores ganacias obtenidas en un periodo de fechas determiando.",
+  },
+];
